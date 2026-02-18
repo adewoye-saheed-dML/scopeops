725438d9e9fb92b6c4db75472b5852c85c79e63b
adewoye-saheed-dML <adewoyesaheed001@gmail.com>
2026-02-18 14:37:07 -0800

    handling dead connection

--- app/database.py
+++ app/database.py
@@ -9,7 +9,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False,
+                        pool_pre_ping=True,   
+                        pool_recycle=1800,   
+                        pool_size=10,         
+                        max_overflow=20)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
